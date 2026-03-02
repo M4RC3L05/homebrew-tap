9484822103e70d7f06fcba65d175658ef3f6b18e
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2026-03-02 19:26:29 +0000

    Update denotag to v4.6.21
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- Formula/denotag.rb
+++ Formula/denotag.rb
@@ -4,21 +4,21 @@
 class Denotag < Formula
   desc "Tag editor built with deno"
   homepage "https://github.com/m4rc3l05/denotag"
-  version "4.6.20"
+  version "4.6.21"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.20/denotag-x86_64-apple-darwin.tar.gz"
-      sha256 "3eab60cce6c55b60f891383e2ca6bcba3b33181c84f3c62ef3c9b38dc025aa9a"
+      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.21/denotag-x86_64-apple-darwin.tar.gz"
+      sha256 "d21b8febe76e95ba7132a12b88e643ef160f039d6883bf63b14914a1fe003a6f"
 
       def install
         bin.install "denotag"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.20/denotag-aarch64-apple-darwin.tar.gz"
-      sha256 "d0f086b1c83b359da6456636f3c48ce60ff5d60a821cb6dd339c06bc63e7f106"
+      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.21/denotag-aarch64-apple-darwin.tar.gz"
+      sha256 "5e5f25b14ad46b11812586c302cda57ccfc6cae4f5a96620ddb3d2c50ea57aa6"
 
       def install
         bin.install "denotag"
@@ -28,15 +28,15 @@ class Denotag < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.20/denotag-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "525218a6a7914e52e89b8b32633296b928bc384800d7339fdfae6a8afeb12c9c"
+      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.21/denotag-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "2e81913f69336ece75701bb778496894ba3b8ee9a047f6b0b5ce7490feeffb22"
       def install
         bin.install "denotag"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.20/denotag-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "5f2a371701a0913b881fc6833f2a7002c1af3cfe30cae7e22013c2fbab81558e"
+      url "https://github.com/M4RC3L05/denotag/releases/download/v4.6.21/denotag-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ab0198d402f80636c66d0984d02269868293e9953f4bd926a0b8eeba78f29a8f"
       def install
         bin.install "denotag"
       end
